9135d1c7385e8dd566517deb0a1697d5dc0e896c
garlic0x1 <garlic1@protonmail.com>
2023-12-07 01:40:58 -0600

    .clang-format sources

--- modules/c.el
+++ modules/c.el
@@ -1,9 +1,16 @@
+(defun clang-format-with-spec (spec)
+  (shell-command
+   (concat "clang-format"
+           " --style=file:" spec
+           " -i " buffer-file-name)))
+
 (defun clang-format ()
   (interactive)
-  (shell-command
-   (concat
-    "clang-format"
-    " --style=file:" (config-path ".clang-format")
-    " -i " buffer-file-name)))
+  (let ((project-spec (concat (projectile-project-root) ".clang-format"))
+        (global-spec (config-path ".clang-format")))
+    (clang-format-with-spec
+     (if (file-exists-p project-spec)
+         project-spec global-spec))))
 
-(add-hook 'after-save-hook #'(lambda () (when c-buffer-is-cc-mode (clang-format))))
+(add-hook 'after-save-hook
+          #'(lambda () (when c-buffer-is-cc-mode (clang-format))))
